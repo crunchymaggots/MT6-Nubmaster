9dc0d2048f8991edd728908a8aa69b70d1c9ff9e
crunchymaggots <97642257+crunchymaggots@users.noreply.github.com>
2024-02-01 00:58:56 -0800

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnOpenOriginal = new System.Windows.Forms.Button();
+            this.btnOpenSND = new System.Windows.Forms.Button();
+            this.lbNewPath = new System.Windows.Forms.Label();
+            this.lbOriginalPath = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbLoop = new System.Windows.Forms.TextBox();
+            this.btnLoopWhole = new System.Windows.Forms.Button();
+            this.btnNoLoop = new System.Windows.Forms.Button();
+            this.btnLoopUseFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(15, 196);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(113, 23);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate new .nub";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Loop Point - in samples:";
+            // 
+            // btnOpenOriginal
+            // 
+            this.btnOpenOriginal.Location = new System.Drawing.Point(15, 25);
+            this.btnOpenOriginal.Name = "btnOpenOriginal";
+            this.btnOpenOriginal.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenOriginal.TabIndex = 7;
+            this.btnOpenOriginal.Text = "Open";
+            this.btnOpenOriginal.UseVisualStyleBackColor = true;
+            this.btnOpenOriginal.Click += new System.EventHandler(this.btnOpenOriginal_Click);
+            // 
+            // btnOpenSND
+            // 
+            this.btnOpenSND.Location = new System.Drawing.Point(15, 88);
+            this.btnOpenSND.Name = "btnOpenSND";
+            this.btnOpenSND.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenSND.TabIndex = 9;
+            this.btnOpenSND.Text = "Open";
+            this.btnOpenSND.UseVisualStyleBackColor = true;
+            this.btnOpenSND.Click += new System.EventHandler(this.btnOpenSND_Click);
+            // 
+            // lbNewPath
+            // 
+            this.lbNewPath.AutoSize = true;
+            this.lbNewPath.Location = new System.Drawing.Point(96, 93);
+            this.lbNewPath.Name = "lbNewPath";
+            this.lbNewPath.Size = new System.Drawing.Size(32, 13);
+            this.lbNewPath.TabIndex = 10;
+            this.lbNewPath.Text = "Path:";
+            // 
+            // lbOriginalPath
+            // 
+            this.lbOriginalPath.AutoSize = true;
+            this.lbOriginalPath.Location = new System.Drawing.Point(96, 30);
+            this.lbOriginalPath.Name = "lbOriginalPath";
+            this.lbOriginalPath.Size = new System.Drawing.Size(35, 13);
+            this.lbOriginalPath.TabIndex = 8;
+            this.lbOriginalPath.Text = "Path: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(239, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Original .nub file (track to copy header data from):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "New .snd file:";
+            // 
+            // tbLoop
+            // 
+            this.tbLoop.Location = new System.Drawing.Point(15, 154);
+            this.tbLoop.Name = "tbLoop";
+            this.tbLoop.Size = new System.Drawing.Size(168, 20);
+            this.tbLoop.TabIndex = 15;
+            // 
+            // btnLoopWhole
+            // 
+            this.btnLoopWhole.Location = new System.Drawing.Point(270, 152);
+            this.btnLoopWhole.Name = "btnLoopWhole";
+            this.btnLoopWhole.Size = new System.Drawing.Size(115, 23);
+            this.btnLoopWhole.TabIndex = 16;
+            this.btnLoopWhole.Text = "Loop Whole Track";
+            this.btnLoopWhole.UseVisualStyleBackColor = true;
+            this.btnLoopWhole.Click += new System.EventHandler(this.btnLoopWhole_Click);
+            // 
+            // btnNoLoop
+            // 
+            this.btnNoLoop.Location = new System.Drawing.Point(391, 152);
+            this.btnNoLoop.Name = "btnNoLoop";
+            this.btnNoLoop.Size = new System.Drawing.Size(75, 23);
+            this.btnNoLoop.TabIndex = 17;
+            this.btnNoLoop.Text = "No Loop";
+            this.btnNoLoop.UseVisualStyleBackColor = true;
+            this.btnNoLoop.Click += new System.EventHandler(this.btnNoLoop_Click);
+            // 
+            // btnLoopUseFile
+            // 
+            this.btnLoopUseFile.Location = new System.Drawing.Point(189, 152);
+            this.btnLoopUseFile.Name = "btnLoopUseFile";
+            this.btnLoopUseFile.Size = new System.Drawing.Size(75, 23);
+            this.btnLoopUseFile.TabIndex = 18;
+            this.btnLoopUseFile.Text = "Use File";
+            this.btnLoopUseFile.UseVisualStyleBackColor = true;
+            this.btnLoopUseFile.Click += new System.EventHandler(this.btnLoopUseFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 246);
+            this.Controls.Add(this.btnLoopUseFile);
+            this.Controls.Add(this.btnNoLoop);
+            this.Controls.Add(this.btnLoopWhole);
+            this.Controls.Add(this.tbLoop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbNewPath);
+            this.Controls.Add(this.btnOpenSND);
+            this.Controls.Add(this.lbOriginalPath);
+            this.Controls.Add(this.btnOpenOriginal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnGenerate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MT6 Nubmaster";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnOpenOriginal;
+        private System.Windows.Forms.Button btnOpenSND;
+        private System.Windows.Forms.Label lbNewPath;
+        private System.Windows.Forms.Label lbOriginalPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbLoop;
+        private System.Windows.Forms.Button btnLoopWhole;
+        private System.Windows.Forms.Button btnNoLoop;
+        private System.Windows.Forms.Button btnLoopUseFile;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog openOriginal = new OpenFileDialog();
+        OpenFileDialog openNew = new OpenFileDialog();
+        OpenFileDialog openLoop = new OpenFileDialog();
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (lbOriginalPath.Text != openOriginal.FileName)  //No .nub file selected
+            {
+                MessageBox.Show("Please select a .nub song to act as a donor file.\n\nDo not worry, it will NOT be overwritten.", "Missing .nub file",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+            if (lbNewPath.Text != openNew.FileName)  //No .snd file selected
+            {
+                MessageBox.Show("Please select a .snd file. Dingus.", "Missing .snd file", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            FileInfo sndInfo = new FileInfo(openNew.FileName);
+            long sampleLengthLong = sndInfo.Length;
+            int sampleLengthInt = Convert.ToInt32(sampleLengthLong);
+            
+            if (tbLoop.Text == "")  //No loop point declared
+            {
+                MessageBox.Show("Please specify (in samples) the point at which the song should loop.", "Loop point missing", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int sampleLoopInt;
+            if (Int32.TryParse(tbLoop.Text, out sampleLoopInt)) { }  //Ensure that the loop value is actually a number
+            else
+            {
+                MessageBox.Show("Not a valid number, dingus.", "C'mon, man", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (sampleLoopInt < 0 || sampleLoopInt > 1000000000)  //Ensure that the loop point is a sensible value
+            {
+                MessageBox.Show("Just don't.", "Son,", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            sampleLoopInt = sampleLoopInt * 4;
+            if (sampleLoopInt > sampleLengthInt)  //Ensure that the loop occurs before the song ends
+            {
+                MessageBox.Show("Your loop point is occurring after the song has already ended.", "Loop occurs after song has ended.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string FILE_NAME = Path.GetFileName(openOriginal.FileName);
+
+            if (File.Exists(FILE_NAME))  //Prevent overwriting of existing files
+            {
+                MessageBox.Show("There is already a .nub file in the same folder as this application with this file name.\n\nThis program will not (intentionally) overwrite existing .nub files for safety. Please move it somewhere else or delete it.", "File Already Exists", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            BinaryReader inputNubFile = new BinaryReader(File.OpenRead(openOriginal.FileName));
+            BinaryReader inputSndFile = new BinaryReader(File.OpenRead(openNew.FileName));
+
+            using (FileStream fs = new FileStream(FILE_NAME, FileMode.CreateNew))
+            {
+                using (BinaryWriter outputNubFile = new BinaryWriter(fs))
+                {
+                    //Clone the header data from source file from 0x00 to 0xFF
+                    for (int i = 0x0; i <= 0xFF; i++)
+                    {
+                        inputNubFile.BaseStream.Position = i;
+                        outputNubFile.Write(inputNubFile.ReadByte());
+                    }
+
+                    //Subtract nessisary value from total file length, and write to header
+                    sampleLengthInt = sampleLengthInt - 1792;
+                    byte[] intLengthBytes = BitConverter.GetBytes(sampleLengthInt);
+                    outputNubFile.BaseStream.Position = 0x44;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        outputNubFile.Write(intLengthBytes[i]);
+                    }
+
+                    //Write loop start point to header
+                    byte[] intLoopBytes = BitConverter.GetBytes(sampleLoopInt);
+                    outputNubFile.BaseStream.Position = 0x50;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        outputNubFile.Write(intLoopBytes[i]);
+                    }
+
+                    //Write loop length to header
+                    int sampleDiff = sampleLengthInt - sampleLoopInt;
+                    if (sampleLoopInt == 0) { sampleDiff = 0; }
+                    byte[] intDiffBytes = BitConverter.GetBytes(sampleDiff);
+                    outputNubFile.BaseStream.Position = 0x54;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        outputNubFile.Write(intDiffBytes[i]);
+                    }
+
+                    //Write Song Data from snd file
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    inputSndFile.BaseStream.Position = 0x0;
+                    outputNubFile.BaseStream.Position = 0x100;
+                    while ((bytesRead = inputSndFile.Read(buffer, 0, 1024)) > 0)
+                    {
+                        outputNubFile.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+            inputNubFile.Close();
+            MessageBox.Show(".nub File Generated", "Operation Commplete", MessageBoxButtons.OK);
+        }
+
+        private void btnOpenOriginal_Click(object sender, EventArgs e)
+        {
+            openOriginal.Filter = "nub File (*.nub)|*.nub";
+            openOriginal.ShowDialog();
+            if (openOriginal.FileName != "")
+            { lbOriginalPath.Text = openOriginal.FileName; }
+            else
+            { lbOriginalPath.Text = "Path:"; }
+        }
+
+        private void btnOpenSND_Click(object sender, EventArgs e)
+        {
+            openNew.Filter = "snd File (*.snd)|*.snd";
+            openNew.ShowDialog();
+            if (openNew.FileName != "")
+            { lbNewPath.Text = openNew.FileName; }
+            else
+            { lbNewPath.Text = "Path:"; }
+        }
+
+        private void btnLoopUseFile_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Instructions: Select a version of your .snd file, where the end of the track is where you want the loop to start from.\n\nIn other words, select a copy of the song that only contains the intro.", "How to Use", MessageBoxButtons.OK);
+            openLoop.Filter = "snd File (*.snd)|*.snd";
+            openLoop.ShowDialog();
+            if (openLoop.FileName != "")
+            {
+                FileInfo sndLoop = new FileInfo(openLoop.FileName);
+                long sampleLoopLong = sndLoop.Length;
+                sampleLoopLong = (sampleLoopLong / 4);
+                string sampleLoopStr = Convert.ToString(sampleLoopLong);
+                tbLoop.Text = sampleLoopStr;
+            }
+        }
+
+        private void btnLoopWhole_Click(object sender, EventArgs e)
+        {
+            if (openNew.FileName == "")
+            {
+                MessageBox.Show("Please select your .snd file first.", "Missing .snd file", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            FileInfo sndFull = new FileInfo(openNew.FileName);
+            long sampleLengthLong = sndFull.Length;
+            sampleLengthLong = (sampleLengthLong / 4);
+            string sampleLengthStr = Convert.ToString(sampleLengthLong);
+            tbLoop.Text = sampleLengthStr;
+
+        }
+
+        private void btnNoLoop_Click(object sender, EventArgs e)
+        {
+            tbLoop.Text = "0";
+        }
+    }
+}
